381b30da69395311a3b1ad26e051298d01e8d113
iandemed <iandemed@gmail.com>
2021-02-08 23:13:59 -0500

    Restructure data, create exploratory chart

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,17 +1,94 @@
-import {combatDamage} from './combat.js'
+async function drawHistogram() {
 
-let damageDice = {
-    num: 1,
-    sides: 4,
+    /*---- Load the data ----*/
+    const dataset = await d3.json("../data/dmgDistributions.json")
+
+    console.log(dataset)
+
+    const metricAccessor = d => d.dmg
+    const yAccessor = d => d.length
+
+    /*---- Create chart dimensions ----*/
+    const width = 600
+
+    let dimensions = {
+      width: width,
+      height: width * 0.6,
+      margin: {
+        top: 30,
+        right: 10,
+        bottom: 50,
+        left: 50,
+      },
+    }
+    dimensions.boundedWidth = dimensions.width
+      - dimensions.margin.left
+      - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height
+      - dimensions.margin.top
+      - dimensions.margin.bottom
+
+    dimensions.boundedWidth = dimensions.width
+        - dimensions.margin.left
+        - dimensions.margin.right
+    
+    dimensions.boundedHeight = dimensions.height
+        - dimensions.margin.top
+        - dimensions.margin.bottom
+   
+    /*---- Draw canvas ----*/
+    const wrapper = d3.select("#wrapper")
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height)
+    
+    const bounds = wrapper.append("g")
+        .style("transform", `translate(${
+                dimensions.margin.left
+            }px, ${
+                dimensions.margin.top
+            }px)`)
+
+    /*---- Create scales ----*/
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, metricAccessor))
+        .range([0, dimensions.boundedWidth])
+        .nice()
+    
+    const binsGenerator = d3.histogram()
+        .domain(xScale.domain())
+        .value(metricAccessor)
+        .thresholds(12)
+    
+    const bins = binsGenerator(dataset)
+    
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(bins, yAccessor)])
+        .range([dimensions.boundedHeight, 0])
+        .nice()
+
+    
+    /*---- Draw the data ----*/    
+    const binsGroup = bounds.append("g")
+
+    const binGroups = binsGroup.selectAll("g")
+      .data(bins)
+      .enter().append("g")
+  
+    const barPadding = 1
+    const barRects = binGroups.append("rect")
+        .attr("x", d => xScale(d.x0) + barPadding / 2)
+        .attr("y", d => yScale(yAccessor(d)))
+        .attr("width", d => d3.max([
+          0,
+          xScale(d.x1) - xScale(d.x0) - barPadding
+        ]))
+        .attr("height", d => dimensions.boundedHeight
+          - yScale(yAccessor(d))
+        )
+        .attr("fill", "cornflowerblue")
+    
+    console.log(bins)
 }
 
-let modifiers = {
-    damage: 0,
-    hit: 0
-}
-
-
-// Test logic for advantage and disadvantage
-console.log(combatDamage(damageDice, modifiers, true, true))
-console.log(combatDamage(damageDice, modifiers, true, false))
-console.log(combatDamage(damageDice, modifiers, false, true))
+drawHistogram()
