9ed9cbe65d9cfac22a9c2f7b44d9e606608ebdbe
Ian de Medeiros <44042532+iandemed@users.noreply.github.com>
2021-03-16 15:49:57 -0400

    Scrubbing working when state variable not being set

--- src/components/shared/ScrubInput/ScrubInput.jsx
+++ src/components/shared/ScrubInput/ScrubInput.jsx
@@ -5,10 +5,63 @@ import './ScrubInput.css'
 const ScrubInput = () => {
 
     const [val, setVal] = useState(12)
+    
+    // Using a side-effect rather than a state variable
+    let lastX = 0
+    
+    const scrub = (e) =>
+    {
+        let num = (e.clientX - lastX)/10
+        console.log(num)
+
+    }
+
+    const handleMouseDown = (e) =>
+    {
+    
+        console.log("Mouse Down")
+        const inputBox=e.target
+
+        lastX = e.clientX
+
+        inputBox.addEventListener('mousemove', scrub)
+        
+
+        e.preventDefault()
+    }
+
+    // Stop dragging once we leave the box to prevent calling and MouseUp event
+    // on the document
+    const handleMouseLeave = (e) => {
+
+        console.log("Mouse Left")
+
+        const input = e.target.children[0]
+        input.blur()
+        
+        const inputBox = e.target
+        inputBox.removeEventListener('mousemove',scrub)
+        
+        e.preventDefault()
+
+        
+    }
+
+    const handleMouseUp = (e) =>{
+
+        console.log("Mouse Up")
+        const inputBox = e.target
+        
+        inputBox.removeEventListener('mousemove',scrub)
+
+        e.preventDefault()
+    }
+
+
 
     const inputFillStyle = {
         height: '100%',
-        backgroundColor: '#d81921',
+        backgroundColor: '#6B6B6B',
         width: `${Math.min((val/35)*100, 100)}%`,
         position: 'absolute',
         zIndex: -1,
@@ -18,7 +71,12 @@ const ScrubInput = () => {
     }
 
     return(
-        <div className="input-container">
+        <div 
+            className="input-container"
+            onMouseDown={handleMouseDown} 
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseLeave} // mouseleave does not bubble, meaning it is fired when pointer has exited element AND all descendants
+        >
             
             <label className="input-label">
                 Test:
@@ -32,10 +90,6 @@ const ScrubInput = () => {
                         setVal(e.target.value)
                         console.log(Math.min(val/35*100, 100))
                     }}
-                    onMouseDown={(e) =>{
-                        console.log("Mouse Down")
-                    }}
-                    style={{}}
                 />
             </label>
             <div style={inputFillStyle}></div>
